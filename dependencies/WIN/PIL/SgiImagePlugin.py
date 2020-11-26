8d7076df1bdd4ddb0cd150edeb1ffe511d1f4aac
Loïc Norgeot <loic.norgeot@sketchfab.com>
2020-11-26 15:43:28 +0100

    [#D3D-5882] Updates PIL Windows dependency

--- dependencies/WIN/PIL/SgiImagePlugin.py
+++ dependencies/WIN/PIL/SgiImagePlugin.py
@@ -22,14 +22,13 @@
 #
 
 
-from . import Image, ImageFile
-from ._binary import i8, o8, i16be as i16
-from ._util import py3
-import struct
 import os
+import struct
 
-
-__version__ = "0.3"
+from . import Image, ImageFile
+from ._binary import i8
+from ._binary import i16be as i16
+from ._binary import o8
 
 
 def _accept(prefix):
@@ -44,7 +43,7 @@ MODES = {
     (1, 3, 3): "RGB",
     (2, 3, 3): "RGB;16B",
     (1, 3, 4): "RGBA",
-    (2, 3, 4): "RGBA;16B"
+    (2, 3, 4): "RGBA;16B",
 }
 
 
@@ -61,8 +60,7 @@ class SgiImageFile(ImageFile.ImageFile):
         headlen = 512
         s = self.fp.read(headlen)
 
-        # magic number : 474
-        if i16(s) != 474:
+        if not _accept(s):
             raise ValueError("Not an SGI image file")
 
         # compression : verbatim or RLE
@@ -98,6 +96,8 @@ class SgiImageFile(ImageFile.ImageFile):
 
         self._size = xsize, ysize
         self.mode = rawmode.split(";")[0]
+        if self.mode == "RGB":
+            self.custom_mimetype = "image/rgb"
 
         # orientation -1 : scanlines begins at the bottom-left corner
         orientation = -1
@@ -106,19 +106,21 @@ class SgiImageFile(ImageFile.ImageFile):
         if compression == 0:
             pagesize = xsize * ysize * bpc
             if bpc == 2:
-                self.tile = [("SGI16", (0, 0) + self.size,
-                              headlen, (self.mode, 0, orientation))]
+                self.tile = [
+                    ("SGI16", (0, 0) + self.size, headlen, (self.mode, 0, orientation))
+                ]
             else:
                 self.tile = []
                 offset = headlen
                 for layer in self.mode:
                     self.tile.append(
-                        ("raw", (0, 0) + self.size,
-                            offset, (layer, 0, orientation)))
+                        ("raw", (0, 0) + self.size, offset, (layer, 0, orientation))
+                    )
                     offset += pagesize
         elif compression == 1:
-            self.tile = [("sgi_rle", (0, 0) + self.size,
-                          headlen, (rawmode, orientation, bpc))]
+            self.tile = [
+                ("sgi_rle", (0, 0) + self.size, headlen, (rawmode, orientation, bpc))
+            ]
 
 
 def _save(im, fp, filename):
@@ -157,8 +159,9 @@ def _save(im, fp, filename):
 
     # assert we've got the right number of bands.
     if len(im.getbands()) != z:
-        raise ValueError("incorrect number of bands in SGI write: %s vs %s" %
-                         (z, len(im.getbands())))
+        raise ValueError(
+            f"incorrect number of bands in SGI write: {z} vs {len(im.getbands())}"
+        )
 
     # Minimum Byte value
     pinmin = 0
@@ -166,31 +169,30 @@ def _save(im, fp, filename):
     pinmax = 255
     # Image name (79 characters max, truncated below in write)
     imgName = os.path.splitext(os.path.basename(filename))[0]
-    if py3:
-        imgName = imgName.encode('ascii', 'ignore')
+    imgName = imgName.encode("ascii", "ignore")
     # Standard representation of pixel in the file
     colormap = 0
-    fp.write(struct.pack('>h', magicNumber))
+    fp.write(struct.pack(">h", magicNumber))
     fp.write(o8(rle))
     fp.write(o8(bpc))
-    fp.write(struct.pack('>H', dim))
-    fp.write(struct.pack('>H', x))
-    fp.write(struct.pack('>H', y))
-    fp.write(struct.pack('>H', z))
-    fp.write(struct.pack('>l', pinmin))
-    fp.write(struct.pack('>l', pinmax))
-    fp.write(struct.pack('4s', b''))  # dummy
-    fp.write(struct.pack('79s', imgName))  # truncates to 79 chars
-    fp.write(struct.pack('s', b''))  # force null byte after imgname
-    fp.write(struct.pack('>l', colormap))
-    fp.write(struct.pack('404s', b''))  # dummy
+    fp.write(struct.pack(">H", dim))
+    fp.write(struct.pack(">H", x))
+    fp.write(struct.pack(">H", y))
+    fp.write(struct.pack(">H", z))
+    fp.write(struct.pack(">l", pinmin))
+    fp.write(struct.pack(">l", pinmax))
+    fp.write(struct.pack("4s", b""))  # dummy
+    fp.write(struct.pack("79s", imgName))  # truncates to 79 chars
+    fp.write(struct.pack("s", b""))  # force null byte after imgname
+    fp.write(struct.pack(">l", colormap))
+    fp.write(struct.pack("404s", b""))  # dummy
 
-    rawmode = 'L'
+    rawmode = "L"
     if bpc == 2:
-        rawmode = 'L;16B'
+        rawmode = "L;16B"
 
     for channel in im.split():
-        fp.write(channel.tobytes('raw', rawmode, 0, orientation))
+        fp.write(channel.tobytes("raw", rawmode, 0, orientation))
 
     fp.close()
 
@@ -205,12 +207,14 @@ class SGI16Decoder(ImageFile.PyDecoder):
         self.fd.seek(512)
 
         for band in range(zsize):
-            channel = Image.new('L', (self.state.xsize, self.state.ysize))
-            channel.frombytes(self.fd.read(2 * pagesize), 'raw',
-                              'L;16B', stride, orientation)
+            channel = Image.new("L", (self.state.xsize, self.state.ysize))
+            channel.frombytes(
+                self.fd.read(2 * pagesize), "raw", "L;16B", stride, orientation
+            )
             self.im.putband(channel.im, band)
 
         return -1, 0
+
 
 #
 # registry
@@ -220,9 +224,7 @@ Image.register_decoder("SGI16", SGI16Decoder)
 Image.register_open(SgiImageFile.format, SgiImageFile, _accept)
 Image.register_save(SgiImageFile.format, _save)
 Image.register_mime(SgiImageFile.format, "image/sgi")
-Image.register_mime(SgiImageFile.format, "image/rgb")
 
-Image.register_extensions(SgiImageFile.format,
-                          [".bw", ".rgb", ".rgba", ".sgi"])
+Image.register_extensions(SgiImageFile.format, [".bw", ".rgb", ".rgba", ".sgi"])
 
 # End of file
--- dependencies/WIN/PIL/_util.py
+++ dependencies/WIN/PIL/_util.py
@@ -1,20 +1,9 @@
 import os
-import sys
+from pathlib import Path
 
-py3 = sys.version_info.major >= 3
 
-if py3:
-    def isStringType(t):
-        return isinstance(t, str)
-
-    def isPath(f):
-        return isinstance(f, (bytes, str))
-else:
-    def isStringType(t):
-        return isinstance(t, basestring)  # noqa: F821
-
-    def isPath(f):
-        return isinstance(f, basestring)  # noqa: F821
+def isPath(f):
+    return isinstance(f, (bytes, str, Path))
 
 
 # Checks if an object is a string, and that it points to a directory.
@@ -22,7 +11,7 @@ def isDirectory(f):
     return isPath(f) and os.path.isdir(f)
 
 
-class deferred_error(object):
+class deferred_error:
     def __init__(self, ex):
         self.ex = ex
 
